da66cdab71ca330cdd969be9fa0370e0f518ff15
Joe Becher <jwbecher@drazisil.com>
2019-11-17 10:17:04 -0500

    start of server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hellp from the DashboardZero backend!");
+});
+
+function start(port = port) {
+  app.listen(port, () => {
+    console.log(`DashboardZero backend listening on port ${port}`);
+  });
+}
+
+module.exports = {
+  start
+};
