c27c8f8574148b06abeabc51d93b5a9e40d2ee15
Joe Becher <jwbecher@drazisil.com>
2019-11-17 10:19:28 -0500

    change response to json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hellp from the DashboardZero backend!");
+  res.json({ message: "Hellp from the DashboardZero backend!" });
 });
 
 function start(port = port) {
